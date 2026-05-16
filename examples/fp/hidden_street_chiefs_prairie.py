dd2de1d89b30fe10c556dbdbb103196cf3bc775a
Hong <qaz9756wsx@gmail.com>
2026-05-16 18:50:12 +0800

    On branch master  Your branch is ahead of 'master/master' by 1 commit.    (use "git push" to publish your local commits)
     Changes to be committed:
    	new file:   data/mini_map_titles/hidden_street_chiefs_prairie.png
    	new file:   examples/fp/hidden_street_chiefs_prairie.py

---
+++ examples/fp/hidden_street_chiefs_prairie.py
@@ -0,0 +1,329 @@
+from chrono_marco2.player import Player
+from chrono_marco2.player.eye import Eye
+from chrono_marco2.common.mini_map_data import MiniMapData
+from chrono_marco2.key_holder import KeyHolderWin
+from chrono_marco2.common import KeyBinds
+from chrono_marco2.keeper import CountdownTimer, FreeMarketKeeper
+
+import alpha_setting
+
+from pathlib import Path
+import time
+import random
+import pyautogui
+
+explosion_hold_time: tuple[float, float] = (0.24, 0.27)
+
+list_explosion_left: list[str] = [
+    alpha_setting.AttackKeys.explosion,
+    KeyBinds.left,
+]
+list_explosion_right: list[str] = [
+    alpha_setting.AttackKeys.explosion,
+    KeyBinds.right,
+]
+
+list_explosion_left_jump: list[str] = [
+    alpha_setting.AttackKeys.explosion,
+    KeyBinds.left,
+    KeyBinds.jump,
+]
+list_explosion_right_jump: list[str] = [
+    alpha_setting.AttackKeys.explosion,
+    KeyBinds.right,
+    KeyBinds.jump,
+]
+
+list_explosion_up_jump: list[str] = [
+    alpha_setting.AttackKeys.explosion,
+    KeyBinds.up,
+    KeyBinds.jump,
+]
+list_explosion_left_up_jump: list[str] = [
+    alpha_setting.AttackKeys.explosion,
+    KeyBinds.left,
+    KeyBinds.up,
+    KeyBinds.jump,
+]
+list_explosion_right_up_jump: list[str] = [
+    alpha_setting.AttackKeys.explosion,
+    KeyBinds.right,
+    KeyBinds.up,
+    KeyBinds.jump,
+]
+
+list_explosion_down_jump: list[str] = [
+    alpha_setting.AttackKeys.explosion,
+    KeyBinds.down,
+    KeyBinds.jump,
+]
+list_explosion_left_down_jump: list[str] = [
+    alpha_setting.AttackKeys.explosion,
+    KeyBinds.left,
+    KeyBinds.down,
+    KeyBinds.jump,
+]
+list_explosion_right_down_jump: list[str] = [
+    alpha_setting.AttackKeys.explosion,
+    KeyBinds.right,
+    KeyBinds.down,
+    KeyBinds.jump,
+]
+
+little_rush: KeyHolderWin = KeyHolderWin(
+    [alpha_setting.AttackKeys.rush],
+    (0.03, 0.06),
+    (0.03, 0.06),
+)
+
+little_left: KeyHolderWin = KeyHolderWin([KeyBinds.left], (0.2, 0.2))
+little_right: KeyHolderWin = KeyHolderWin([KeyBinds.right], (0.2, 0.2))
+little_up: KeyHolderWin = KeyHolderWin([KeyBinds.up], (0.03, 0.06), (0.3, 0.3))
+
+right_prev_jump: KeyHolderWin = alpha_setting.attack_prev_jump(
+    direction_keys=[KeyBinds.right]
+)
+left_prev_jump: KeyHolderWin = alpha_setting.attack_prev_jump(
+    direction_keys=[KeyBinds.left]
+)
+
+right_down_prev_jump: KeyHolderWin = alpha_setting.attack_prev_jump(
+    direction_keys=[KeyBinds.right, KeyBinds.down]
+)
+
+left_down_prev_jump: KeyHolderWin = alpha_setting.attack_prev_jump(
+    direction_keys=[KeyBinds.left, KeyBinds.down]
+)
+
+explosion_right: KeyHolderWin = KeyHolderWin(
+    [
+        alpha_setting.AttackKeys.explosion,
+        KeyBinds.right,
+    ],
+    explosion_hold_time,
+)
+
+explosion_left: KeyHolderWin = KeyHolderWin(
+    [
+        alpha_setting.AttackKeys.explosion,
+        KeyBinds.left,
+    ],
+    explosion_hold_time,
+)
+
+explosion_right_rush: KeyHolderWin = KeyHolderWin(
+    [
+        alpha_setting.AttackKeys.explosion,
+        KeyBinds.right,
+        KeyBinds.rush,
+    ],
+    explosion_hold_time,
+)
+
+explosion_left_rush: KeyHolderWin = KeyHolderWin(
+    [
+        alpha_setting.AttackKeys.explosion,
+        KeyBinds.left,
+        KeyBinds.rush,
+    ],
+    explosion_hold_time,
+)
+
+explosion_down_right_jump: KeyHolderWin = KeyHolderWin(
+    [
+        alpha_setting.AttackKeys.explosion,
+        KeyBinds.down,
+        KeyBinds.right,
+        KeyBinds.jump,
+    ],
+    explosion_hold_time,
+)
+
+explosion_down_left_jump: KeyHolderWin = KeyHolderWin(
+    [
+        alpha_setting.AttackKeys.explosion,
+        KeyBinds.down,
+        KeyBinds.left,
+        KeyBinds.jump,
+    ],
+    explosion_hold_time,
+)
+
+explosion_down_right_jump_rush: KeyHolderWin = KeyHolderWin(
+    [
+        alpha_setting.AttackKeys.explosion,
+        KeyBinds.down,
+        KeyBinds.right,
+        KeyBinds.jump,
+        KeyBinds.rush,
+    ],
+    explosion_hold_time,
+)
+
+explosion_down_left_jump_rush: KeyHolderWin = KeyHolderWin(
+    [
+        alpha_setting.AttackKeys.explosion,
+        KeyBinds.down,
+        KeyBinds.left,
+        KeyBinds.jump,
+        KeyBinds.rush,
+    ],
+    explosion_hold_time,
+)
+
+explosion_right_jump_rush: KeyHolderWin = KeyHolderWin(
+    [
+        alpha_setting.AttackKeys.explosion,
+        KeyBinds.right,
+        KeyBinds.jump,
+        KeyBinds.rush,
+    ],
+    explosion_hold_time,
+)
+
+explosion_left_jump_rush: KeyHolderWin = KeyHolderWin(
+    [
+        alpha_setting.AttackKeys.explosion,
+        KeyBinds.left,
+        KeyBinds.jump,
+        KeyBinds.rush,
+    ],
+    explosion_hold_time,
+)
+
+explosion_little_up: KeyHolderWin = KeyHolderWin(
+    [
+        alpha_setting.AttackKeys.explosion,
+        KeyBinds.up,
+    ],
+    (0.03, 0.06),
+    (0.3, 0.3),
+)
+
+explosion_little_left: KeyHolderWin = KeyHolderWin(
+    [
+        alpha_setting.AttackKeys.explosion,
+        KeyBinds.left,
+    ],
+    (0.03, 0.06),
+)
+
+explosion_little_right: KeyHolderWin = KeyHolderWin(
+    [
+        alpha_setting.AttackKeys.explosion,
+        KeyBinds.right,
+    ],
+    (0.03, 0.06),
+)
+
+
+def how_to_play(player: Player):
+    match player.eye.status.current_yellow_point_position_in_mini_map:
+
+        
+        # # platform 0
+        # case (x, y) if 19 <= x <= 63 and 33 <= y <= 48:
+        #     with pyautogui.hold(list_explosion_left_down_jump):
+        #         for _ in range(2):
+        #             little_rush.hold()
+        
+        # # platform 1
+        # case (x, y) if 70 <= x <= 130 and 25 <= y <= 40:
+        #     with pyautogui.hold(list_explosion_left_down_jump):
+        #         for _ in range(2):
+        #             little_rush.hold()
+        
+
+        # platform 2
+        case (x, y) if 12 <= x <= 71 and 58 <= y <= 72:
+            with pyautogui.hold(list_explosion_left):
+                for _ in range(5):
+                    little_rush.hold()
+
+        # platform 3
+        case (x, y) if 70 <= x <= 130 and 49 <= y <= 63:
+            with pyautogui.hold(list_explosion_left):
+                for _ in range(5):
+                    little_rush.hold()
+        
+        # platform 5
+        case (x, y) if 175 <= x <= 225 and 64 <= y <= 77:
+            with pyautogui.hold(list_explosion_left_up_jump):
+                for _ in range(2):
+                    little_rush.hold()
+
+        # platform 4
+        # case (x, y) if 138 <= x <= 182 and 52 <= y <= 68:
+        case (x, y) if 131 <= x <= 182 and 52 <= y <= 68:
+            with pyautogui.hold(list_explosion_left_jump):
+                for _ in range(2):
+                    little_rush.hold()
+        
+        # platform 6 right right
+        case (x, y) if 200 <= x <= 236 and 82 <= y <= 97:
+            with pyautogui.hold(list_explosion_left_up_jump):
+                for _ in range(2):
+                    little_rush.hold()
+        
+        # platform 6 right
+        case (x, y) if 175 <= x <= 236 and 82 <= y <= 97:
+            with pyautogui.hold(list_explosion_right_up_jump):
+                for _ in range(2):
+                    little_rush.hold()
+        
+        # platform 6
+        case (x, y) if 7 <= x <= 236 and 82 <= y <= 97:
+            with pyautogui.hold(list_explosion_right):
+                for _ in range(5):
+                    little_rush.hold()
+
+        case (x, y):
+            if random.random() < 0.5:
+                with pyautogui.hold(list_explosion_right_down_jump):
+                    for _ in range(2):
+                        little_rush.hold()
+            else:
+                with pyautogui.hold(list_explosion_left_down_jump):
+                    for _ in range(2):
+                        little_rush.hold()
+
+        case _:
+            if random.random() < 0.5:
+                with pyautogui.hold(list_explosion_right_down_jump):
+                    for _ in range(2):
+                        little_rush.hold()
+            else:
+                with pyautogui.hold(list_explosion_left_down_jump):
+                    for _ in range(2):
+                        little_rush.hold()
+
+    for keeper in player.keepers:
+        keeper.do_on_finish()
+
+
+if __name__ == "__main__":
+    path = Path().cwd() / "data" / "mini_map_titles" / "hidden_street_chiefs_prairie.png"
+    eye: Eye = Eye(
+        path,
+        MiniMapData.hidden_street_chiefs_prairie["title"],
+        MiniMapData.hidden_street_chiefs_prairie["region"],
+    )
+
+    free_market_keeper: FreeMarketKeeper = alpha_setting.BuffKeepers.free_market
+    free_market_keeper.duration = 1200
+    take_a_break_keeper: FreeMarketKeeper = alpha_setting.BuffKeepers.take_a_break
+    take_a_break_keeper.refresh()
+    take_a_break_keeper.refresh_other_free_market_keepers = [free_market_keeper]
+
+    keepers: list[CountdownTimer] = [
+        take_a_break_keeper,
+        alpha_setting.BuffKeepers.skill_buffs,
+        alpha_setting.BuffKeepers.pills,
+        alpha_setting.BuffKeepers.sugar_rush_candy,
+        free_market_keeper,
+    ]
+    player = Player(eye=eye, keepers=keepers)
+
+    pyautogui.hotkey("alt", "tab")
+    time.sleep(1)
+    player.run(how_to_play, pre_do_keepers=True)
