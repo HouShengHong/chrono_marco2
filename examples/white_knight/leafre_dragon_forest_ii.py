f4e235eff7de1cd1a41a033a81fcaedb70787b67
hong <qaz9756wsx@gmail.com>
2026-05-23 11:27:35 +0800

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   examples/white_knight/leafre_dragon_forest_ii.py

--- examples/white_knight/leafre_dragon_forest_ii.py
+++ examples/white_knight/leafre_dragon_forest_ii.py
@@ -88,13 +88,13 @@ def how_to_play(player: Player):
 
         # platform 2 teleport point left
         case (x, y) if 181 <= x <= 187 and 93 <= y <= 113:
-            little_right.hold()
             lightning_attack.hold()
+            little_right.hold()
 
         # platform 2 teleport point right
         case (x, y) if 193 <= x <= 199 and 93 <= y <= 113:
-            little_left.hold()
             lightning_attack.hold()
+            little_left.hold()
 
         # platform 2 teleport point far left
         case (x, y) if 174 <= x <= 180 and 93 <= y <= 113:
