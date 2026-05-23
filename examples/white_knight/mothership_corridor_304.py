cd562cca3bb3e5396b6142a50b06abcb9bd2ebf0
hong <qaz9756wsx@gmail.com>
2026-05-23 10:58:58 +0800

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   examples/white_knight/mothership_corridor_304.py

--- examples/white_knight/mothership_corridor_304.py
+++ examples/white_knight/mothership_corridor_304.py
@@ -106,7 +106,7 @@ def how_to_play(player: Player):
         player.hand.status = 2
     elif 0 <= player.eye.status.current_yellow_point_position_in_mini_map[0] <= 25:
         player.hand.status = 2
-    elif 225 <= player.eye.status.current_yellow_point_position_in_mini_map[0] <= 248:
+    elif 236 <= player.eye.status.current_yellow_point_position_in_mini_map[0] <= 248:
         player.hand.status = 1
 
     match player.eye.status.current_yellow_point_position_in_mini_map:
