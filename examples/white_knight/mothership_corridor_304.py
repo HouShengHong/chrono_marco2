31a65277009340c64727508f3e5dbd0731d573da
hong <qaz9756wsx@gmail.com>
2026-05-24 01:10:59 +0800

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   examples/white_knight/mothership_corridor_304.py

--- examples/white_knight/mothership_corridor_304.py
+++ examples/white_knight/mothership_corridor_304.py
@@ -127,9 +127,12 @@ def how_to_play(player: Player):
         # platform 1
         case (x, y) if 58 <= y <= 71:
             if player.hand.status == 1:
-                left_prev_jump.hold()
-                lightning_attack.hold()
+                # left_prev_jump.hold()
+                # lightning_attack.hold()
+                # left_lightning_rush.hold()
+                left_mid_jump.hold()
                 left_lightning_rush.hold()
+                lightning_attack.hold()
 
             else:
                 left_down_prev_jump.hold()
@@ -138,9 +141,12 @@ def how_to_play(player: Player):
         # platform 2
         case (x, y) if 73 <= y <= 90:
             if player.hand.status == 2:
-                right_prev_jump.hold()
-                lightning_attack.hold()
+                # right_prev_jump.hold()
+                # lightning_attack.hold()
+                # right_lightning_rush.hold()
+                right_small_jump.hold()
                 right_lightning_rush.hold()
+                lightning_attack.hold()
             else:
                 left_big_jump.hold()
                 left_lightning_rush.hold()
