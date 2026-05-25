9d66ca2a80d0861fc289e7893a5e2424f4ac47e2
hong <qaz9756wsx@gmail.com>
2026-05-25 23:24:50 +0800

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   examples/white_knight/mothership_corridor_304.py

--- examples/white_knight/mothership_corridor_304.py
+++ examples/white_knight/mothership_corridor_304.py
@@ -134,6 +134,7 @@ def how_to_play(player: Player):
             else:
                 left_down_prev_jump.hold()
                 lightning_attack.hold()
+                right_lightning_rush.hold()
 
         # platform 2
         case (x, y) if 73 <= y <= 90:
