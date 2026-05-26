91893fd1447c22f07b0424bbf8bf3b71ed11e972
hong <qaz9756wsx@gmail.com>
2026-05-27 02:27:47 +0800

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   examples/white_knight/leafre_the_burning_forest.py

--- examples/white_knight/leafre_the_burning_forest.py
+++ examples/white_knight/leafre_the_burning_forest.py
@@ -129,7 +129,6 @@ def how_to_play(player: Player):
         # platform 1
         case (x, y) if 69 <= x <= 150 and 44 <= y <= 57:
             left_lightning_rush.hold()
-            left_lightning_rush.hold()
             left_down_prev_jump.hold()
             lightning_attack.hold()
 
