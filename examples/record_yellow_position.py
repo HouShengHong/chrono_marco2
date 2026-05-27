99f04090f35d91b5e88b4ee289d59bfcf17479c2
hong <qaz9756wsx@gmail.com>
2026-05-27 09:33:58 +0800

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   examples/record_yellow_position.py

--- examples/record_yellow_position.py
+++ examples/record_yellow_position.py
@@ -6,8 +6,7 @@ from chrono_marco2.player.ear import Ear
 from chrono_marco2.common.mini_map_data import MiniMapData
 
 if __name__ == "__main__":
-    path = Path().cwd() / "data" / "mini_map_titles" / "mothership_corridor_304"
-    print(path)
+    path = Path().cwd() / "data" / "mini_map_titles" / "mothership_corridor_304.png"
     eye: Eye = Eye(
         path,
         MiniMapData.mothership_corridor_304["title"],
