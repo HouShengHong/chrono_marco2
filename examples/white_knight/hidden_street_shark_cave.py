b04dee917f1b5bf6faf8f470dbc0f2016fd1b972
hong <qaz9756wsx@gmail.com>
2026-05-28 03:27:18 +0800

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   examples/white_knight/hidden_street_shark_cave.py

--- examples/white_knight/hidden_street_shark_cave.py
+++ examples/white_knight/hidden_street_shark_cave.py
@@ -97,7 +97,6 @@ def how_to_play(player: Player):
                 lightning_attack.hold()
             else:
                 left_lightning_rush.hold()
-                left_prev_jump.hold()
                 lightning_attack.hold()
 
         case (x, y):
