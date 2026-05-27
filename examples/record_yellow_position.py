559fbc61090383df1a605e437e6b49608de00e18
hong <qaz9756wsx@gmail.com>
2026-05-27 09:23:55 +0800

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   examples/record_yellow_position.py

--- examples/record_yellow_position.py
+++ examples/record_yellow_position.py
@@ -1,17 +1,16 @@
 from pathlib import Path
 import pyautogui
 import time
-import datetime
 from chrono_marco2.player.eye import Eye
 from chrono_marco2.player.ear import Ear
 from chrono_marco2.common.mini_map_data import MiniMapData
 
 if __name__ == "__main__":
-    path = Path().cwd() / "data" / "mini_map_titles" / "hidden_street_chiefs_prairie.png"
+    path = Path().cwd() / "data" / "mini_map_titles" / "mothership_corridor_304"
     eye: Eye = Eye(
         path,
-        MiniMapData.hidden_street_chiefs_prairie["title"],
-        MiniMapData.hidden_street_chiefs_prairie["region"],
+        MiniMapData.mothership_corridor_304["title"],
+        MiniMapData.mothership_corridor_304["region"],
     )
     ear = Ear()
 
@@ -25,7 +24,10 @@ if __name__ == "__main__":
         # print(f"Same map: {s}", datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S:%f"))
         p = eye.status.current_yellow_point_position_in_mini_map
         p = (int(p[0]), int(p[1])) if p else None
-        match (s, p,):
+        match (
+            s,
+            p,
+        ):
             case (True, p):
                 if p is not None:
                     temp_set.add(p)
@@ -33,7 +35,7 @@ if __name__ == "__main__":
                 print(None)
 
     pyautogui.hotkey("alt", "tab")
-    
+
     temp_list = list(temp_set)
     sorted_0 = sorted(temp_list, key=lambda x: (x[0], x[1]))
     sorted_1 = sorted(temp_list, key=lambda x: (x[1], x[0]))
@@ -44,7 +46,3 @@ if __name__ == "__main__":
     if len(temp_list) > 0:
         print(sorted_0[0], sorted_0[-1])
         print(sorted_1[0], sorted_1[-1])
-
-
-    
-
