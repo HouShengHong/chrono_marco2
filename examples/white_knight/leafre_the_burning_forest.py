7e11c465ed5383584874acf0224903e394094279
hong <qaz9756wsx@gmail.com>
2026-05-28 04:31:02 +0800

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   examples/white_knight/leafre_the_burning_forest.py

--- examples/white_knight/leafre_the_burning_forest.py
+++ examples/white_knight/leafre_the_burning_forest.py
@@ -164,14 +164,14 @@ if __name__ == "__main__":
         alpha_setting.BuffKeepers.pills,
     ]
 
-    # free_market_keeper: FreeMarketKeeper = alpha_setting.BuffKeepers.free_market
-    # free_market_keeper.duration = 1200
+    free_market_keeper: FreeMarketKeeper = alpha_setting.BuffKeepers.free_market
+    free_market_keeper.duration = 1200
     take_a_break_keeper: FreeMarketKeeper = alpha_setting.BuffKeepers.take_a_break
     take_a_break_keeper.refresh()
-    # take_a_break_keeper.refresh_other_free_market_keepers = [free_market_keeper]
+    take_a_break_keeper.refresh_other_free_market_keepers = [free_market_keeper]
 
     keepers: list[CountdownTimer] = [
-        take_a_break_keeper,
+        # take_a_break_keeper,
         alpha_setting.BuffKeepers.lightning_charge,
         alpha_setting.BuffKeepers.skill_buffs,
         alpha_setting.BuffKeepers.pills,
