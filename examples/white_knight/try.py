5ef96e4b962f446eaec1351a3712cf3c1ae5c2c3
Hong <qaz9756wsx@gmail.com>
2026-06-04 23:16:43 +0800

    On branch master  Your branch is up to date with 'master/master'.
     Changes to be committed:
    	modified:   examples/white_knight/try.py

--- examples/white_knight/try.py
+++ examples/white_knight/try.py
@@ -33,15 +33,13 @@ if __name__ == "__main__":
         end_sleep_time=(0.49, 0.49),
     )
 
+    key_test:KeyHolderWin = KeyHolderWin(["j"],(59.999,59.999),(0,0))
+
     pyautogui.hotkey("alt", "tab")
     time.sleep(1)
-    i = 5
-    for _ in range(i):
-        for _ in range(i):
-            right_lightning_rush.hold()
-
-        for _ in range(i):
-            left_lightning_rush.hold()
-
+    time1 = time.time()
+    key_test.hold()
+    time2 = time.time()
+    print(time2 - time1)
     time.sleep(1)
     pyautogui.hotkey("alt", "tab")
